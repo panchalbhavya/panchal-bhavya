d120547c1ac46f6fe0a26b067d0fab4e2fc42433
panchalbhavya <240130510032.cs@gujaratvidyapith.org>
2026-04-03 12:35:31 +0530

    Integrate Google Analytics tracking script
    Added Google Analytics tracking code to the header.

--- index.php
+++ index.php
@@ -5,7 +5,15 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SVWP278E2B"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-SVWP278E2B');
+</script>
 <header>
 <h1>Smile Dental Clinic</h1>
 <nav>
